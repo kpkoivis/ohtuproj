87cdb9effdf2a0c4b8c0bb8dd770c119a65d6592
Kristian Koivisto <kristiak@cs.helsinki.fi>
2014-05-16 15:53:38 +0300

    tilakonetta rakentamassa

--- app/assets/javascripts/code.js
+++ app/assets/javascripts/code.js
@@ -2,9 +2,237 @@
  * Created by kristiak on 14.5.2014.
  */
 
+function stateMachine () {
+    function state (name, entryFunc, eventFunc) {
+        var name = name;
+        var entryFunc = entryFunc;
+        var eventFunc = eventFunc;
+
+        return {
+            name : name,
+            entryFunc : entryFunc,
+            eventFunc : eventFunc
+        };
+    }
+
+    function transition (fromState, toState, keys) {
+        var fromState = fromState;
+        var toState = toState;
+        var keys = keys;
+
+        return {
+            fromState : fromState,
+            toState : toState,
+            keys : keys
+        };
+    }
+
+
+    var states = [ ];
+    var stateIndex = 0;
+
+    var transitions = [ ];
+    var transitionIndex = 0;
+
+    var state;
+
+
+    function reset() {
+        state = 0;
+        states[0].entryFunc();
+    }
+
+    function getCurrentStateNumber() {
+        return state;
+    }
+
+    function getCurrentStateName() {
+        var stat = getCurrentStateNumber();
+        return states[stat];
+    }
+
+
+    function setStateByNumber(newState) {
+        state = newState;
+    }
+
+    function setStateByName(newStateName) {
+        state = states.indexOf(newStateName);
+    }
+
+    function registerState(newStateName, entryfunction, eventFunction) {
+        var newState = new state (newStateName, entryfunction, eventFunction);
+        states[stateIndex++] = newState;
+    }
+
+    function registerTransition(fromState, toState, keys) {
+        var newTransition = new transition(fromState, toState, keys);
+        transitions[transitionIndex++] = newTransition;
+    }
+
+    function doEvent(eventInformation) {
+        states[state].eventFunc(eventInformation);
+    }
+
+    function transition(stateName) {
+        state = 1;
+        states[1].entryFunc();
+    }
+
+    return {
+        reset : reset,
+        getCurrentStateNumber : getCurrentStateNumber,
+        getCurrentStateName : getCurrentStateName,
+        setStateByNumber : setStateByNumber,
+        setStateByName : setStateByName,
+        registerState : registerState,
+        registerTransition : registerTransition,
+        doEvent : doEvent,
+        transition : transition
+    };
+}
+
+
+
 var game = {};
 
-game.stmtest = (function() {
+game.stmtest = {};
+
+
+
+
+game.stmtest.logic = (function() {
+
+    var instructions;
+    var number;
+    var input;
+    var stateElement;
+    instructions = $( "#instructions" );
+    number = document.getElementById("dispNumber");
+    input = document.getElementById("input");
+    stateElement = document.getElementById("state");
+
+    var stateM = new stateMachine();
+
+    stateM.registerState("start",           doStateStart, doEventStart);
+    stateM.registerState("displayNumbers",  doStateDisplayNumbers, doEventDisplayNumbers);
+    stateM.registerState("intersession",    doStateIntersession, doEventIntersession);
+    stateM.registerState("userInput",       doStateUserInput, doEventUserInput);
+    stateM.registerState("endInfo",         doStateEndInfo, doEventEndInfo);
+
+    stateM.registerTransition("start", "displayNumbers", " ");
+    stateM.registerTransition("displayNumbers", "intersession", "");
+    stateM.registerTransition("intersession", "userInput", " ");
+    stateM.registerTransition("userInput", "userInput", "0123456789");
+    stateM.registerTransition("userInput", "endInfo", "H");
+    stateM.registerTransition("endInfo", "start", " ");
+
+    function doStateStart() {
+        inputChars = [ ];
+        inputIndex = 0;
+        document.getElementById("instructions").innerHTML = "Kohta näytetään numeroita. Paina jotain nappulaa aloittaaksesi";
+    }
+
+    function doStateDisplayNumbers() {
+        instructions.innerHTML = "No nyt tulee!";
+
+        displayIndex = 0;
+        showNumIntervalFunc = setInterval(showNumber, 1000);
+    }
+
+    function doStateIntersession() {
+        instructions.innerHTML = "Tää on välivaihe! Paina nappulaa niin saat luetella numerot!";
+    }
+
+    function doStateUserInput() {
+        instructions.innerHTML = "Ny on numeroiden syöttö meneillään! Paine h-näppäintä kun olet valmis!";
+    }
+
+    function doStateEndInfo() {
+        var s = "";
+        s += "Nää merkit sää laitoit: ";
+        for (var i = 0; i < inputChars.length; i++) {
+            s += "- " + inputChars[i];
+        }
+        s += " Paina jotain näppäintä jatkaaksesi!";
+        instructions.innerHTML = s;
+    }
+
+    function showNumber() {
+        if (stateM.getCurrentStateName() == "displayNumbers") {
+            if (displayIndex == displayNumbers.length) {
+                number.innerHTML = " ";
+                clearInterval(showNumIntervalFunc);
+                setStateByName("intersession");
+                doState();
+            } else {
+                number.innerHTML = displayNumbers[displayIndex];
+                displayIndex++;
+            }
+        }
+    }
+
+
+
+
+
+    function doEventStart(eventInformation) {
+        stateM.transition("displayNumbers");
+    }
+
+    function doEventDisplayNumbers(eventInformation) {
+
+    }
+
+    function doEventIntersession(eventInformation) {
+        setStateByName("userInput");
+        doState();
+    }
+
+    function doEventUserInput(eventInformation) {
+        if (String.fromCharCode(eventInformation.which) == "H") {
+            setStateByName("endInfo");
+            doState();
+        } else {
+            inputChars[inputIndex++] = String.fromCharCode(eventInformation.which);
+        }
+    }
+
+    function doEventEndInfo(eventInformation) {
+        setStateByName("start");
+        doState();
+
+    }
+
+    function reset () {
+        stateM.reset();
+
+
+        instructions = document.getElementById("instructions");
+        number = document.getElementById("dispNumber");
+        input = document.getElementById("input");
+        stateElement = document.getElementById("state");
+    }
+
+    function doEvent(eventInformation) {
+        stateM.doEvent(eventInformation);
+        //doEventFunc[getCurrentStateName()](eventInformation);
+        input.innerHTML = ": " + String.fromCharCode(eventInformation.which);
+    }
+
+
+
+    return {
+        reset : reset,
+        doEvent : doEvent
+    };
+
+
+})();
+
+
+
+game.stmtest.ui = (function() {
 
     var instructions;
     var number;
@@ -37,23 +265,7 @@ game.stmtest = (function() {
 
     var showNumIntervalFunc;
 
-    function getCurrentStateNumber() {
-        return state;
-    }
 
-    function getCurrentStateName() {
-        var stat = getCurrentStateNumber();
-        return states[stat];
-    }
-    
-
-    function setStateByNumber(newState) {
-        state = newState;
-    }
-
-    function setStateByName(newStateName) {
-        state = states.indexOf(newStateName);
-    }
 
 
 
@@ -167,11 +379,11 @@ game.stmtest = (function() {
 
 $(document).ready(function() {
     document.getElementById("state").innerHTML = "ABOUT TO RESET";
-    game.stmtest.reset();
-    game.stmtest.doState();
+    game.stmtest.logic.reset();
+    //game.stmtest.logic.start();
 
     $(document).keydown(function(eventInformation) {
-        game.stmtest.doEvent(eventInformation);
+        game.stmtest.logic.doEvent(eventInformation);
     });
 });
 
