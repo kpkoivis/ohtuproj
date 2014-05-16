440ba757c3fd2c36f4e02ca315b094ebb9ef8daa
Kristian Koivisto <kristiak@cs.helsinki.fi>
2014-05-16 11:34:45 +0300

    toimii

--- app/assets/javascripts/code.js
+++ app/assets/javascripts/code.js
@@ -6,60 +6,66 @@ var game = {};
 
 game.stmtest = (function() {
 
-    var selite = document.getElementById("selite");
-    var number = document.getElementById("numero");
-    var input = document.getElementById("input");
-    var stateElement = document.getElementById("state");
+    var instructions;
+    var number;
+    var input;
+    var stateElement;
 
     //var displayNumbers = [ "7", "6", "5", "4", "3"];
     var displayNumbers = [ "7", "6" ];
+    var displayIndex;
 
     var inputChars = [ ];
-    var inputIndex = 0;
+    var inputIndex;
     
-    var states = [ "start", "displayNumbers" ];
-    var state = 0;
+    var states = [ "start", "displayNumbers", "intersession", "userInput", "endInfo" ];
+    var state;
 
     var showNumIntervalFunc;
 
     function getCurrentStateNumber() {
-        return [state];
+        return state;
     }
 
     function getCurrentStateName() {
-        return states[getCurrentStateNumber()];
+        var stat = getCurrentStateNumber();
+        return states[stat];
     }
     
-    
-    
-    function setStateNumber(newState) {
-        
+
+    function setStateByNumber(newState) {
         state = newState;
     }
-    
+
+    function setStateByName(newStateName) {
+        state = states.indexOf(newStateName);
+    }
+
+
+
     function doEvent(eventInformation) {
-        var curState = getCurrentStateNumber();
+        var curState = getCurrentStateName();
 
         if (curState == "start") {
-            setStateNumber("displayNumbers");
+            setStateByName("displayNumbers");
             doState();
         }
 
-        else if (curState == "valiVaihe") {
-            setStateNumber("syotaNumerot");
+        else if (curState == "intersession") {
+            setStateByName("userInput");
             doState();
         }
 
-        else if (curState == "syotaNumerot") {
+        else if (curState == "userInput") {
             if (String.fromCharCode(eventInformation.which) == "H") {
-                setStateNumber("loppuinfo");
+                setStateByName("endInfo");
                 doState();
             } else {
                 inputChars[inputIndex++] = String.fromCharCode(eventInformation.which);
             }
         }
 
-        else if (state == "loppuinfo") {
+        else if (state == "endInfo") {
 
         }
 
@@ -67,75 +73,69 @@ game.stmtest = (function() {
     }
 
     function doState() {
-        var curState = getCurrentStateNumber();
+        var curState = getCurrentStateName();
         stateElement.innerHTML = curState;
 
-
         if (curState == "start") {
-            selite.innerHTML = "Kohta näytetään numeroita. Paina jotain nappulaa aloittaaksesi";
+            instructions.innerHTML = "Kohta näytetään numeroita. Paina jotain nappulaa aloittaaksesi";
         }
 
         if (curState == "displayNumbers") {
-            selite.innerHTML = "No nyt tulee!";
+            instructions.innerHTML = "No nyt tulee!";
 
+            displayIndex = 0;
             showNumIntervalFunc = setInterval(showNumber, 1000);
         }
 
-        if (curState == "valiVaihe") {
-            selite.innerHTML = "Tää on välivaihe! Paina nappulaa niin saat luetella numero!";
+        if (curState == "intersession") {
+            instructions.innerHTML = "Tää on välivaihe! Paina nappulaa niin saat luetella numerot!";
 
         }
 
-        if (curState == "syotaNumerot") {
-            selite.innerHTML = "Ny on numeroiden syöttö meneillään! Paine h-näppäintä kun olet valmis!";
+        if (curState == "userInput") {
+            instructions.innerHTML = "Ny on numeroiden syöttö meneillään! Paine h-näppäintä kun olet valmis!";
         }
 
-        if (curState == "loppuinfo") {
-            number.innerHTML = "KUKKUU!";
+        if (curState == "endInfo") {
             var s = "";
             s += "Nää merkit sää laitoit: ";
             for (var i = 0; i < inputChars.length; i++) {
                 s += "- " + inputChars[i];
             }
-            selite.innerHTML = s;
+            instructions.innerHTML = s;
         }
 
         function showNumber() {
-            if (curState == "displayNumbers") {
-                if (index == displayNumbers.length) {
+            if (getCurrentStateName() == "displayNumbers") {
+                if (displayIndex == displayNumbers.length) {
                     number.innerHTML = " ";
                     clearInterval(showNumIntervalFunc);
-                    setStateNumber("valiVaihe");
+                    setStateByName("intersession");
                     doState();
                 } else {
-                    number.innerHTML = displayNumbers[index];
-                    index++;
+                    number.innerHTML = displayNumbers[displayIndex];
+                    displayIndex++;
                 }
             }
         }
     }
 
-
     function reset () {
-        state = 0;
+        setStateByName("start");
+
         inputChars = [ ];
         inputIndex = 0;
-        document.getElementById("state").innerHTML = "RESET";
+
+        instructions = document.getElementById("instructions");
+        number = document.getElementById("dispNumber");
+        input = document.getElementById("input");
+        stateElement = document.getElementById("state");
     }
-
-
 
     return {
         reset : reset,
-        doState : function () {
-            dostate();
-        },
-        getCurrentStateNumber : function () {
-            name = newName;
-        },
-        putEvent : function (eventInformation) {
-            doEvent(eventInformation);
-        }
+        doState : doState,
+        doEvent : doEvent
     };
 })();
 
@@ -143,12 +143,11 @@ game.stmtest = (function() {
 
 $(document).ready(function() {
     document.getElementById("state").innerHTML = "ABOUT TO RESET";
-    //var thisGame = new game();
     game.stmtest.reset();
     game.stmtest.doState();
 
     $(document).keydown(function(eventInformation) {
-        game.stmtest.putEvent(eventInformation);
+        game.stmtest.doEvent(eventInformation);
     });
 });
 
